e4c3e9453e278a6a1d35ffe6a48888d6e661152d
Yiwei Yu <yiweiyu32@gmail.com>
2024-07-16 09:41:51 -0500

    Update tests

--- tests/test_amortization_table_generator.py
+++ tests/test_amortization_table_generator.py
@@ -3,7 +3,7 @@ import pandas as pd
 from src.amortization_table_generator import amortization_table_generator
 
 principal = 30000.0  # Loan amount
-annual_interest_rate = 0.07  # Annual interest rate in percentage
+annual_interest_rate = 0.11  # Annual interest rate in percentage
 term = 30  # Loan term in years
 loan_start_date = datetime.date(2024, 8, 1)  # Loan start date
 
